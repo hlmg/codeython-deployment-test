e68f17e707fc9bea9bd71f46974678fd56178564
ZENO <zeno1030@naver.com>
2024-04-11 14:35:18 +0900

    init setting

---
+++ src/main/java/clofi/codeython/CodeythonApplication.java
@@ -0,0 +1,13 @@
+package clofi.codeython;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeythonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeythonApplication.class, args);
+	}
+
+}
