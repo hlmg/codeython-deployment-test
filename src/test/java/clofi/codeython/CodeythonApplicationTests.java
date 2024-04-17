1e8332284feaac5da6287c98bd153c734a8a2859
Lee <94841127+galaxian@users.noreply.github.com>
2024-04-17 13:57:22 +0900

    feat: 테스트 코드 주석 제거

--- src/test/java/clofi/codeython/CodeythonApplicationTests.java
+++ src/test/java/clofi/codeython/CodeythonApplicationTests.java
@@ -1,13 +1,13 @@
-// package clofi.codeython;
-//
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-//
-// @SpringBootTest
-// class CodeythonApplicationTests {
-//
-// 	@Test
-// 	void contextLoads() {
-// 	}
-//
-// }
+package clofi.codeython;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeythonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
