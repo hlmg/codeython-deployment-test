8f9394b6e34a5409a21f2027cc806605ddab0864
ZENO <zeno1030@naver.com>
2024-04-19 18:15:11 +0900

    refactor. cors 수정 및 레벨 및 경험치 수정

--- src/main/java/clofi/codeython/member/domain/Member.java
+++ src/main/java/clofi/codeython/member/domain/Member.java
@@ -21,41 +21,38 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member extends BaseEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_no", nullable = false)
-	private Long userNo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_no", nullable = false)
+    private Long userNo;
 
-	@Column(name = "username", nullable = false, length = 20)
-	private String username;
+    @Column(name = "username", nullable = false, length = 20)
+    private String username;
 
-	@Column(name = "password", nullable = false, length = 128)
-	private String password;
+    @Column(name = "password", nullable = false, length = 128)
+    private String password;
 
-	@Column(name = "nickname", nullable = false, length = 10)
-	private String nickname;
+    @Column(name = "nickname", nullable = false, length = 10)
+    private String nickname;
 
-	@Column(name = "level", nullable = false, columnDefinition = "int default 1")
-	private Integer level;
+    @Column(name = "exp", nullable = false, columnDefinition = "int default 0")
+    private Integer exp;
 
-	@Column(name = "exp", nullable = false, columnDefinition = "int default 0")
-	private Integer exp;
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
-	@Column(name = "deleted_at")
-	private LocalDateTime deletedAt;
+    public Member(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
 
-	public Member(String username, String password, String nickname) {
-		this.username = username;
-		this.password = password;
-		this.nickname = nickname;
-	}
+    public Member(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-	public Member(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-
-	public void updateNickName(String nickname) {
-		this.nickname = nickname;
-	}
+    public void updateNickName(String nickname) {
+        this.nickname = nickname;
+    }
 }
--- src/main/java/clofi/codeython/member/service/MemberService.java
+++ src/main/java/clofi/codeython/member/service/MemberService.java
@@ -13,7 +13,6 @@ import clofi.codeython.member.domain.request.CreateMemberRequest;
 import clofi.codeython.member.domain.request.UpdateMemberRequest;
 import clofi.codeython.member.repository.MemberRepository;
 import clofi.codeython.member.service.dto.CustomMemberDetails;
-import clofi.codeython.problem.repository.RecordRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
@@ -21,45 +20,48 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional
 public class MemberService implements UserDetailsService {
-	private final MemberRepository memberRepository;
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-	private final RecordRepository recordRepository;
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public Long signUp(CreateMemberRequest createMemberRequest) {
-		if (memberRepository.existsByNickname(createMemberRequest.getNickname())) {
-			throw new IllegalArgumentException("이미 존재한 닉네임입니다.");
-		}
-		if (memberRepository.existsByUsername(createMemberRequest.getUsername())) {
-			throw new IllegalArgumentException("이미 존재하는 아이디 입니다.");
-		}
-		Member member = memberRepository.save(createMemberRequest.toMember(bCryptPasswordEncoder));
-		return member.getUserNo();
-	}
+    public Long signUp(CreateMemberRequest createMemberRequest) {
+        if (memberRepository.existsByNickname(createMemberRequest.getNickname())) {
+            throw new IllegalArgumentException("이미 존재한 닉네임입니다.");
+        }
+        if (memberRepository.existsByUsername(createMemberRequest.getUsername())) {
+            throw new IllegalArgumentException("이미 존재하는 아이디 입니다.");
+        }
+        Member member = memberRepository.save(createMemberRequest.toMember(bCryptPasswordEncoder));
+        return member.getUserNo();
+    }
 
-	public Long update(String userName, UpdateMemberRequest updateMemberRequest) {
-		Member memberId = memberRepository.findByUsername(userName);
-		Member member = memberRepository.findByUserNo(memberId.getUserNo())
-			.orElseThrow(() -> new EntityNotFoundException("일치하는 사용자가 없습니다."));
-		if (memberRepository.existsByNickname(updateMemberRequest.getNickname())) {
-			throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
-		}
-		member.updateNickName(updateMemberRequest.getNickname());
-		return member.getUserNo();
-	}
+    public Long update(String userName, UpdateMemberRequest updateMemberRequest) {
+        Member memberId = memberRepository.findByUsername(userName);
+        Member member = memberRepository.findByUserNo(memberId.getUserNo())
+            .orElseThrow(() -> new EntityNotFoundException("일치하는 사용자가 없습니다."));
+        if (memberRepository.existsByNickname(updateMemberRequest.getNickname())) {
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
+        member.updateNickName(updateMemberRequest.getNickname());
+        return member.getUserNo();
+    }
 
-	public MemberResponse getMember(String userName) {
-		Member member = memberRepository.findByUsername(userName);
-		return memberRepository.findByUserNo(member.getUserNo())
-			.map(MemberResponse::of)
-			.orElseThrow(() -> new EntityNotFoundException("일치하는 사용자가 없습니다."));
-	}
+    public MemberResponse getMember(String userName) {
+        Member member = memberRepository.findByUsername(userName);
+        Integer exp = member.getExp();
+        int level = 1;
+        if (exp > 100) {
+            level = exp / 100;
+            exp = exp % 100;
+        }
+        return MemberResponse.of(member, level, exp);
+    }
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Member memberData = memberRepository.findByUsername(username);
-		if (memberData != null) {
-			return new CustomMemberDetails(memberData);
-		}
-		return null;
-	}
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member memberData = memberRepository.findByUsername(username);
+        if (memberData != null) {
+            return new CustomMemberDetails(memberData);
+        }
+        return null;
+    }
 }
